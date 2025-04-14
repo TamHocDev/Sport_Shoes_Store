f4df8e47fefb8a9176ebf65eb400b00cabb8ce43
tungduong2610 <accsohaduong@gmail.com>
2025-04-14 23:42:09 +0700

    duong

--- app/src/main/java/com/example/shopgiaythethao/Activity/IntroActivity.java
+++ app/src/main/java/com/example/shopgiaythethao/Activity/IntroActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
